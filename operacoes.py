47d1068f9cabaf9add3f096dbe5febef8222be89
augusto88-0 <augusto.guimaraesantos@gmail.com>
2025-09-07 21:34:00 -0300

    operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,10 @@
+# Desafio 3: Operações matemáticas
+# Objetivo: somar, subtrair, multiplicar e dividir dois números
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
