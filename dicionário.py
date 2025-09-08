d07ec8cd8295769a57330ef6e6a2989131db7076
augusto88-0 <augusto.guimaraesantos@gmail.com>
2025-09-07 21:45:57 -0300

    dicionário.py

---
+++ dicionário.py
@@ -0,0 +1,12 @@
+# Desafio 9: Dicionários
+# Objetivo: armazenar informações de um aluno
+
+aluno = {
+    "nome": "João",
+    "idade": 14,
+    "nota": 8.5
+}
+
+print("Nome:", aluno["nome"])
+print("Idade:", aluno["idade"])
+print("Nota:", aluno["nota"])
